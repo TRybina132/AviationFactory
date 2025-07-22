cff7e53c626e3639dc72d078ca7158e894d8a2ed
TRybina132 <rybina@managed-code.com>
2025-07-22 17:52:10 +0300

    add data initializer

--- AviationFactory/Entities/Products/Planes/FighterPlane.cs
+++ AviationFactory/Entities/Products/Planes/FighterPlane.cs
@@ -5,6 +5,6 @@ public class FighterPlane : BasePlane
     public List<string> PrimaryRole { get; set; }
     public double CombatRadius { get; set; }
     public double ArmamentCapacity { get; set; }
-    public double SupercruiseCapable { get; set; }
+    public bool SupercruiseCapable { get; set; }
     public List<string> Armament { get; set; }
 }
